9116a2b71e3199a8bf788c3d51bb4d050b8f6f42
Shivam Maniya <shivammaniya@shivams-mbp-2.home>
2025-03-21 20:49:38 -0400

    Fix : settings.py - postGres sql added

--- Backend/server_location_map/settings.py
+++ Backend/server_location_map/settings.py
@@ -73,6 +73,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'server_location_map.urls'
 
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -95,11 +97,19 @@ WSGI_APPLICATION = 'server_location_map.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING'),
+        conn_max_age=600,  # Optimizes connections
+        ssl_require=True   # Ensures SSL for Azure PostgreSQL
+    )
 }
 
 APPEND_SLASH = False
